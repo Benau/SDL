73c95a7c2cb0fc244b11c81554e39afed487f5bd
Benau <Benau@users.noreply.github.com>
2022-11-10 12:35:44 +0800

    Use STKEditText for text edit

--- android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android-project/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -315,6 +315,25 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
         mCurrentNativeState = NativeState.INIT;
     }
 
+    // STK addition
+    public static void moveView(int height)
+    {
+        if (mSurface == null)
+            return;
+        mSurface.setTranslationY(height);
+    }
+
+    public static void reFocusAfterSTKEditText()
+    {
+        if (mSurface == null)
+            return;
+        mSurface.setFocusable(true);
+        mSurface.setFocusableInTouchMode(true);
+        mSurface.requestFocus();
+        mSurface.setOnKeyListener(mSurface);
+        mSurface.setOnTouchListener(mSurface);
+    }
+
     // Setup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
